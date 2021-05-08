2949f67be378f28046627ff33af04db9c1be47d6
selinsenturk2 <83191357+selinsenturk2@users.noreply.github.com>
2021-05-08 19:20:07 +0300

    Bu benim ilk projem

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety = tip güvenliği
+            //Do not repeat yourself - Kendini tekrarlama 
+            //kategorieTİKETİ = değer tutucu, alias
+
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = false;
+
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi butonu");
+            }
+
+            if (sistemeGirisYapmisMi == true) 
+            {
+                Console.WriteLine("Kullanıcı ayarlar butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+            
+           
+           
+
+
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
